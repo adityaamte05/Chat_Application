9af72cb991788de3319553755b09c63424a56340
Aditya Amte <aditya2000amte@gmail.com>
2024-02-20 11:32:38 +0530

    test commit

---
+++ Chat_Application/src/main/java/com/aditya/chatapp/ChatWebApplicationUsingWebsocketApplication.java
@@ -0,0 +1,14 @@
+package com.aditya.chatapp;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatWebApplicationUsingWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatWebApplicationUsingWebsocketApplication.class, args);
+	}
+
+}
